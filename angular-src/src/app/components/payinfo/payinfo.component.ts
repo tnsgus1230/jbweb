9228f53ecb4ef64ba319a90dddc4422d940a1f68
slackid <shade1105@naver.com>
2019-11-25 23:57:33 +0900

    결제시스템 코드작성중 업데이트

--- angular-src/src/app/components/payinfo/payinfo.component.ts
+++ angular-src/src/app/components/payinfo/payinfo.component.ts
@@ -56,4 +56,6 @@ export class PayinfoComponent implements OnInit {
       return false;
     }
   }
+
+
 }
---
+++ angular-src/src/app/models/Paylog.ts
@@ -0,0 +1,10 @@
+export class User {
+    name: string;
+    email: string;
+    username: string;
+}
+export class Card {
+    cardnumber: string;
+    expire: string;
+    secretnum: number;
+}
