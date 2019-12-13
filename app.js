aaeaf68b55cd40d971ab812bc991bd00353f3957
slackid <shade1105@naver.com>
2019-12-13 11:55:03 +0900

    porting

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use("/payments", payments);
 
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public"));
 });
 
 app.listen(port, () => {
