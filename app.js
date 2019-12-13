dbd9d03d16ba9460fbc8053e4d388f0b816ef0e8
slackid <shade1105@naver.com>
2019-12-13 12:01:45 +0900

    test

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
-const express = require("express");
-const path = require("path");
-const bodyparser = require("body-parser");
-const cors = require("cors");
-const passport = require("passport");
-const mongoose = require("mongoose");
-const users = require("./routes/users");
-const addbor = require("./routes/addbor");
-const payments = require("./routes/payments");
-const config = require("./config/database");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.connect(config.database, { useMongoClient: true });
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database ' + config.database);
+});
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error ' + err);
+});
 
 const app = express();
 
+const users = require('./routes/users');
+const addbor = require('./routes/addbor');
+const payments = require('./routes/payments');
 
+
+// Port Number
 const port = process.env.PORT || 3000;
 
-mongoose.connect(config.database, { useNewUrlParser: true });
-mongoose.connection.on("connected", () => {
-  console.log("Connected to Database " + config.database);
-});
-mongoose.connection.on("error", err => {
-  console.log("Database error: " + err);
-});
-
+// CORS Middleware
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Body Parser Middleware
+app.use(bodyParser.json());
 
-app.use(bodyparser.json());
-
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
-app.use("/users", users);
+app.use('/users', users);
 app.use("/addbor", addbor);
 app.use("/payments", payments);
-
 
 // Index Route
 app.get('/', (req, res) => {
   res.send('invaild endpoint');
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// Start Server
 app.listen(port, () => {
-  console.log(`server started on port ${port}!`);
+  console.log('Server started on port ' + port);
 });
